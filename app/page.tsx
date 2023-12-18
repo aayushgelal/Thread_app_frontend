23a4af171d44f9743dda4fbf1fdf57a3e905de3a
unknown <aayushgelal@gmail.com>
2023-12-18 09:15:55 +0545

    login front_page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   const { setTheme } = useTheme();
+  const router=useRouter()
+  const loggedUser = useSelector((state: any) => state.user);
+  if (!loggedUser.username) {
+    router.push("/login");
+  }else{
+    router.push("/home")
+  }
   setTheme("dark");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button className=" bg-white text-black" placeholder="Hello"></Button>
     </main>
   );
 }
